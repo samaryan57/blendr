7bb0edaedddc36502a370e4176d40e098eddb889
samaryan57 <samanta2@purdue.edu>
2023-07-26 02:09:34 +0530

    Create login and register pages, connect them to backend and secure the homepage

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,53 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+
 const LoginPage = () => {
-    return (<div>loginpage</div>);
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    return (
+        <Box>
+            <Box
+                width="100%"
+                backgroundColor={alt}
+                p="1rem 6%"
+                textAlign="center"
+            >
+                <Typography
+                    fontWeight="bold"
+                    fontSize="32px"
+                    color="primary"
+                >
+                    Sociopedia
+                </Typography>
+            </Box>
+
+            <Box
+                width={isNonMobileScreens ? "50%" : "93%"}
+                p="2rem"
+                m="2rem auto"
+                borderRadius="1.5rem"
+                backgroundColor={alt}
+            >
+                <Typography
+                    fontWeight="500"
+                    variant="h5"
+                    sx={{
+                        mb: "1.5rem",
+                    }}
+                >
+                    Welcome to Sociopedia, the Social Media for Sociopaths!
+                </Typography>
+                <Form />
+            </Box>
+        </Box>
+    );
 };
 
 export default LoginPage;
