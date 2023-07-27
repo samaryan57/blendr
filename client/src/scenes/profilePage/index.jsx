a8a8b98a63296ecb2a4d29b41f6def268f9ad793
samaryan57 <samanta2@purdue.edu>
2023-07-28 01:59:41 +0530

    Complete home page base structure with adding the ad widget and friend list widget

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,4 +1,16 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostWidget from "scenes/widgets/PostWidget";
+import UserWidget from "scenes/widgets/UserWidget";
+
+
 const ProfilePage = () => {
+    
     return (<div>profilepage</div>);
 };
 
