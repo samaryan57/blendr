b569f6c3f23b8310ee067f49e827c9a2070fd9f4
samaryan57 <samanta2@purdue.edu>
2023-07-28 02:45:11 +0530

    Complete basic skeleton of the web app, add profile page

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -38,7 +38,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
                 <UserImage image={userPicturePath} size="55px" />
                 <Box
                     onClick={() => {
-                        navigate(`profile/${friendId}`);
+                        navigate(`/profile/${friendId}`);
                         navigate(0);
                     }}
                 >
