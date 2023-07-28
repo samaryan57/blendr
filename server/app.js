3385bd3876ad104a32b5e7f1a29293289402c058
samaryan57 <samanta2@purdue.edu>
2023-07-29 04:04:15 +0530

    Update cors policy to allow cross-origin requests between frontend and backend

--- server/app.js
+++ server/app.js
@@ -30,8 +30,25 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+const allowedOrigins = [
+    "https://blendr-taupe.vercel.app",
+    "https://sociopedia-pi.vercel.app"
+];
+  
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+  
 
 /* FILE STORAGE */
 
