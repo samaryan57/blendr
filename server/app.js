74b9c8576958955e9e18533e35b5d44afda97d28
samaryan57 <samanta2@purdue.edu>
2023-07-30 22:27:19 +0530

    Add server modification for cors

--- server/app.js
+++ server/app.js
@@ -59,21 +59,25 @@ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 //     maxAge: 86400, // 1 day (in seconds)
 // };
 
-// app.use(cors());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//     );
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     if (req.method === "OPTIONS") {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-  
 
 /* FILE STORAGE */
 
