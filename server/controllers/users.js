3c0dbc1058b5659b7c64ff288445760046d1309a
samaryan57 <samanta2@purdue.edu>
2023-07-31 11:48:59 +0530

    Debug more

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -8,7 +8,7 @@ export const getUser = async (req, res) => {
         const user = await User.findById(id);
         res.status(200).json(user);
     } catch (err) {
-        res.status(404).json({ message: err.message });
+        res.status(500).json({ message: "caused by mongodb" });
     }
 };
 
